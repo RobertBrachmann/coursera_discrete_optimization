f86c20408eb62acd4a91a1ca6d4932263aaed36d
Poncho <robert.brachmann@web.de>
2019-03-12 21:44:38 +0100

    initial commit

--- week_1_knapsack/solver.py
+++ week_1_knapsack/solver.py
@@ -5,11 +5,60 @@ from collections import namedtuple
 Item = namedtuple("Item", ['index', 'value', 'weight', 'ratio'])
 
 
+def branch_and_bound(items, estimation, search_strategy="df", sorted=False):
+    """
+    Simple branch and bound algorithm with different search strategies
+    :param items:
+    :param search_strategy: df: depth first, bf: best first, ld: least discrepancy
+    :return:value, weight, taken, opt
+    """
+
+    # variables
+    value = 0
+    weight = 0
+    taken = [0] * len(items)
+    opt = 0
+
+    # sort items by value weight ratio
+    if sorted:
+        pass
+
+    # branch and bound algorithms with different strategies
+    if search_strategy == "df":
+        # depth first search
+        for item in items:
+            pass
+
+    elif search_strategy == "bf":
+        pass
+    elif search_strategy == "ld":
+        pass
+
+    return value, weight, taken, opt
+
+
+def linear_relaxation(items):
+    """
+    Relaxes capacity constraint and calculates optimistic estimation
+    :param items: list of items
+    :return: optimistic estimation
+    """
+
+    # optimistic estimation
+    estimation = 0
+
+    # sum all values
+    for item in items:
+        estimation += item.value
+
+    return estimation
+
+
 def value_weight_heuristic(items, capacity):
     """
     Heuristic based on value-weight-ratio
-    :param items:
-    :param capacity:
+    :param items: list of items
+    :param capacity: constraint
     :return:
     """
 
@@ -32,8 +81,6 @@ def value_weight_heuristic(items, capacity):
     # sort items by ratio
     items_w_ratio.sort(key=lambda i: i.ratio, reverse=True)
 
-    print(capacity, items_w_ratio)
-
     for item in items_w_ratio:
         if weight + item.weight <= capacity:
             taken[item.index] = 1
@@ -43,26 +90,7 @@ def value_weight_heuristic(items, capacity):
     return value, weight, taken, opt
 
 
-def linear_relaxation(items):
-    """
-    Relaxes capacity constraint and calculates optimistic estimation
-    :param items: list of items
-    :return: optimistic estimation
-    """
-
-    # optimistic estimation
-    estimation = 0
-
-    # sum all values
-    for item in items:
-        estimation += item.value
-
-    return estimation
-
-
-
-
-def solve_it(input_instance):
+def solve_it(input_instance, instance_location=None):
     # Modify this code to run your optimization algorithm
 
     # parse the input
@@ -82,6 +110,10 @@ def solve_it(input_instance):
 
     # first greedy heuristics
     value, weight, taken, opt = value_weight_heuristic(items, capacity)
+    print(instance_location, items)
+    # estimation by linear relaxation
+    estimation = linear_relaxation(items)
+    value, weight, taken, opt = branch_and_bound(items, estimation, search_strategy="df")
 
     # prepare the solution in the specified output format
     output_data = str(value) + ' ' + str(opt) + '\n'
@@ -96,7 +128,7 @@ if __name__ == '__main__':
         file_location = sys.argv[1].strip()
         with open(file_location, 'r') as input_data_file:
             input_data = input_data_file.read()
-        print(solve_it(input_data))
+        print(solve_it(input_data, file_location))
     else:
         print('This test requires an input file.  '
               'Please select one from the data directory. '
